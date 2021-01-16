383abe56fa130bc0aa5fbf2e4fd759597345f05b
sandra <60742092+sandra33660@users.noreply.github.com>
2021-01-16 16:48:58 +0100

    initialisation de tout les microservices et, des methodes get du microservice catalog

---
+++ src/Services/Catalog/Catalog.API/Models/Interfaces/ITripRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Models
+{
+    public interface ITripRepository : IDisposable
+    {
+        Task<IEnumerable<Trip>> GetTrip(int pageSize, int pageNum);
+        Task<Trip> GetTripById(int idTrip);
+        Task<Trip> InsertTrip(string Details, string Title, DateTime StartDate, DateTime FinalDate, int Price, int NumberOfParticipants);
+        Task<Trip> DeleteTrip(int idTrip);
+        Task<Trip> UpdateTrip(int idTrip);
+    }
+}
---
+++ src/Services/ConnectionManagement/ConnectionManagement.API/Models/Dtos/Authentification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectionManagement.API.Models.Dtos
+{
+    public class Authentification
+    {
+        public int IdAuthentification { get; set; }
+        public int IdCustomer { get; set; }
+        public string Email { get; set; }
+    }
+}
